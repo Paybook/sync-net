73ae66a803b33c2add97b73650d309d8b981020d
Aldo Escobar <aldo.ea@hotmail.com>
2020-05-22 15:24:38 -0500

    Remove Iv & Key

--- Paybook/Paybook/Session.cs
+++ Paybook/Paybook/Session.cs
@@ -12,8 +12,6 @@ namespace PaybookSDK
         public Session() : base() { }
         public User user { get; set; }
         public string token { get; set; }
-        public string iv { get; set; }
-        public string key { get; set; }
         public Session init(User user, string token = "")
         {
             try
@@ -21,16 +19,12 @@ namespace PaybookSDK
                 if (!string.IsNullOrEmpty(token))
                 {
                     this.token = token;
-                    this.iv = string.Empty;
-                    this.key = string.Empty;
                 }
                 else
                 {
                     JObject data = new JObject(new JProperty("api_key", api_key), new JProperty("id_user", user.id_user));
                     JObject response = call("sessions", method.post, data);
                     this.token = response["response"]["token"].ToString();
-                    this.iv = response["response"]["iv"].ToString();
-                    this.key = response["response"]["key"].ToString();
                 }
                 return this;
             }
